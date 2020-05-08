0300048ab3d6caee861c89606d1e49a2ffb93856
MehmetEskar <jvictorjose60@gmail.com>
2020-05-08 20:20:21 +0200

    update practice.java

--- src/practice.java
+++ src/practice.java
@@ -1,7 +1,19 @@
+import java.sql.SQLOutput;
+
 public class practice {
 
     public static void main(String[] args) {
-        System.out.println("just for practise");
+        System.out.println("just for prac");
+
+        System.out.println("hey update me");
+
 
     }
 }
+
+
+
+
+
+
+
