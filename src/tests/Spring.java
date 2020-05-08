31c91ffb94b241fbef4ca6e19b190061b4cb34d7
MehmetEskar <jvictorjose60@gmail.com>
2020-05-08 20:51:13 +0200

    update Spring.java

---
+++ src/tests/Spring.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Spring {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome Spring");
+    }
+
+
+}
