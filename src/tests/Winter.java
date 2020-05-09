e4ea97a78cb5b3c157a5ef646afc6fb2232e01a1
MehmetEskar <jvictorjose60@gmail.com>
2020-05-09 23:36:41 +0200

    Winter.java

---
+++ src/tests/Winter.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Winter {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("winter is coming");
+
+    }
+
+}
